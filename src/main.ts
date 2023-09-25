a3ec114fca3e520f2677e1966b52f0e015e0aee8
youthfulhps <ybh942002@gmail.com>
2023-09-25 23:23:00 +0900

    feature: Implement prettier plugin for v3

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-export * from '~/example'
+import type { Plugin } from 'prettier';
+import { parsers } from './parsers';
+import { printers } from './printers';
+
+const spellingCheckerPlugin: Plugin = {
+  parsers,
+  printers,
+};
+
+export default spellingCheckerPlugin;
