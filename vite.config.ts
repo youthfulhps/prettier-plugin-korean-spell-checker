af26a37d808b203f99114b0f199091e81420c15c
youthfulhps <ybh942002@gmail.com>
2023-09-25 23:17:29 +0900

    feature: Add vite config rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,48 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import { pascalCase } from 'change-case'
-import vitePluginDts from 'vite-plugin-dts'
-import pkg from './package.json'
+import path from 'path';
+import { defineConfig } from 'vite';
+import { pascalCase } from 'change-case';
+import vitePluginDts from 'vite-plugin-dts';
+import pkg from './package.json';
 
 export default defineConfig(({ mode }) => {
-    const isDev = getModeFromEnv(mode) === 'development'
+  const isDev = getModeFromEnv(mode) === 'development';
 
-    return {
-        build: {
-            lib: {
-                entry: path.resolve(__dirname, 'src/main.ts'),
-                name: pascalCase(pkg.name.split('/').pop() ?? ''),
-                fileName: 'lib',
-                formats: ['cjs', 'es', 'umd'],
-            },
-            outDir: 'dist',
-            sourcemap: isDev,
-            /**
-             * @see https://rollupjs.org/configuration-options/#watch
-             */
-            watch: isDev ? {} : null,
-        },
-        resolve: {
-            alias: {
-                '~': path.resolve(__dirname, 'src'),
-            },
-        },
-        plugins: [
-            vitePluginDts({
-                insertTypesEntry: true,
-            }),
-        ],
-    }
-})
+  return {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/main.ts'),
+        name: pascalCase(pkg.name.split('/').pop() ?? ''),
+        fileName: 'lib',
+        formats: ['cjs', 'es', 'umd'],
+      },
+      outDir: 'dist',
+      sourcemap: isDev,
+      /**
+       * @see https://rollupjs.org/configuration-options/#watch
+       */
+      watch: isDev ? {} : null,
+      rollupOptions: {
+        external: ['prettier', '@prettier/sync'],
+      },
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, 'src'),
+      },
+    },
+    plugins: [
+      vitePluginDts({
+        insertTypesEntry: true,
+      }),
+    ],
+  };
+});
 
 function getModeFromEnv(mode: unknown) {
-    switch (mode) {
-        case 'development':
-            return mode
-        default:
-            return 'production'
-    }
+  switch (mode) {
+    case 'development':
+      return mode;
+    default:
+      return 'production';
+  }
 }
