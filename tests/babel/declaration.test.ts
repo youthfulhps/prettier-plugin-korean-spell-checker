4d60a58bc45bc5750a7ef021c297d00877623455
youthfulhps <ybh942002@gmail.com>
2023-10-03 00:21:48 +0900

    test: Add test cases for declaration name

---
+++ tests/babel/declaration.test.ts
@@ -0,0 +1,25 @@
+import type { Fixture } from '../setting';
+import { format } from 'prettier3';
+import { baseOptions } from '../setting';
+
+const options = {
+  ...baseOptions,
+  parser: 'babel',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'babel/declaration',
+    input: `const 안되 = "안돼";`,
+    output: `const 안되 = "안돼";
+`,
+  },
+];
+
+describe('babel/declaration', () => {
+  for (const { name, input, output } of fixtures) {
+    test(name, async () => {
+      expect(await format(input, options)).toBe(output);
+    });
+  }
+});
